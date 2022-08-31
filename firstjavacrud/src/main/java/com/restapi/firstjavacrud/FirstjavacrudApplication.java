f3c0fede9e47ba83f5a039688b661eb63e950c62
allan-gilber <allangilber@yahoo.com.br>
2022-08-31 18:17:22 -0300

    reset: remade project to work with springboot

---
+++ firstjavacrud/src/main/java/com/restapi/firstjavacrud/FirstjavacrudApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.firstjavacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstjavacrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstjavacrudApplication.class, args);
+	}
+
+}
