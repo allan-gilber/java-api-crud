94894b4ca1a62ab7f72ee990a6fb041c4d898186
allan-gilber <allangilber@yahoo.com.br>
2022-09-12 23:28:40 -0300

    rework: reworked get for /user to use errorhandler

--- firstjavacrud/src/main/java/com/restapi/firstjavacrud/methods/UserREST.java
+++ firstjavacrud/src/main/java/com/restapi/firstjavacrud/methods/UserREST.java
@@ -1,7 +1,9 @@
 package com.restapi.firstjavacrud.methods;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,21 +12,42 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.restapi.firstjavacrud.database.RepositoryUserData;
 import com.restapi.firstjavacrud.entitys.UserData;
 import com.restapi.firstjavacrud.exceptions.ApiRequestException;
 
-@RestController
+@/**
+  * This class is a REST controller that handles the requests for the user data
+  */
+RestController
 @RequestMapping("/user")
 public class UserREST {
     @Autowired
     private RepositoryUserData base;
 
+    /**
+     * It's a function that returns a list of users from the database, and it's
+     * paginated
+     * 
+     * @param page The page number to retrieve.
+     * @return A list of users.
+     */
     @GetMapping
-    public ResponseEntity<?> listAll(Pageable pageable) {
-        throw new ApiRequestException("Opps, a error ocurred!");
-        // return new ResponseEntity<>(base.findAll(pageable), HttpStatus.OK);
+    public ResponseEntity<?> listAll(@RequestParam(defaultValue = "0") int page) {
+        try {
+            Page<UserData> request = base.findAll(PageRequest.of(page, 10, Sort.by(Sort.Direction.ASC, "name")));
+            System.out.println(request.getNumberOfElements());
+
+            if (request.getNumberOfElements() == 0)
+                throw new ApiRequestException("Opps, nothing was found. Empty Database", HttpStatus.NOT_FOUND);
+
+            return new ResponseEntity<>(request.getContent(), HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println("Uncaught error: " + e.getMessage());
+            throw new ApiRequestException("Internal server error.");
+        }
     }
 
     @PostMapping
